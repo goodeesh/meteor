3e9ae89b6205673a79db0840e4f6996f756dcc8e
Nacho Codoñer <igcogi@gmail.com>
2025-03-26 17:49:15 +0100

    remove nested imports (to-revert)

--- tools/tests/apps/compiler-plugin-add-asset/use-asset.js
+++
@@ -1,3 +0,0 @@
-(async () => {
-  console.log("Asset says", await Assets.getTextAsync("foo.printme"));
-})();
--- tools/tests/apps/compiler-plugin-asset-and-source/packages/asset-and-source/asset-and-source.js
+++
@@ -1,6 +0,0 @@
-if (Meteor.isServer) {
-  // Printing out my own source code!
-  (async () => {
-    console.log(await Assets.getTextAsync("asset-and-source.js"));
-  })();
-}
